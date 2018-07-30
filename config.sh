adc35ac4a52c302624ce5c32cbfcd4d5682fd00d
philip-a <philip.arthur.om0@is.naist.jp>
2018-07-30 16:16:33 +0900

    Added config

---
+++ config.sh
@@ -0,0 +1,17 @@
+sentpiece=/home/is/philip-a/tools/sentencepiece
+xnmt=/home/is/philip-a/tools/xnmt
+vocab_size=32000
+max_len=50
+min_len=4
+seed=230137
+
+sp_out=data/sp_model
+sp_iwslt=data/sp_model/iwslt
+sp_iwsltenar=data/sp_model/iwsltenar
+sp_kftt=data/sp_model/kftt
+
+
+travatar=/home/is/philip-a/tools/travatar
+bootstrap_sample=10000
+
+fast_align=/home/is/philip-a/tools/fast_align
